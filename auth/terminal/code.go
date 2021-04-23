88d1e3c1c10be9fb329a32670ffdee0c0416b2e5
tdakkota <tanc13@yandex.ru>
2021-04-23 17:26:02 +0300

    feat(terminal): add terminal localization

--- auth/terminal/code.go
+++ auth/terminal/code.go
@@ -7,15 +7,21 @@ import (
 	"strings"
 
 	"golang.org/x/term"
+	"golang.org/x/text/message"
 	"golang.org/x/xerrors"
 
 	"github.com/gotd/td/telegram"
 	"github.com/gotd/td/tg"
+
+	"github.com/gotd/contrib/auth/localization"
 )
+
+var _ telegram.UserAuthenticator = (*Terminal)(nil)
 
 // Terminal implements UserAuthenticator.
 type Terminal struct {
 	*term.Terminal
+	printer *message.Printer
 }
 
 // New creates new Terminal.
@@ -29,12 +35,19 @@ func New(in io.Reader, out io.Writer) *Terminal {
 	}
 	return &Terminal{
 		Terminal: term.NewTerminal(rw, ""),
+		printer:  localization.DefaultPrinter(),
 	}
 }
 
 // OS creates new Terminal using os.Stdout and os.Stdin.
 func OS() *Terminal {
 	return New(os.Stdin, os.Stdout)
+}
+
+// WithPrinter sets localization printer.
+func (t *Terminal) WithPrinter(printer *message.Printer) *Terminal {
+	t.printer = printer
+	return t
 }
 
 func (t *Terminal) read(prompt string) (string, error) {
@@ -45,27 +58,27 @@ func (t *Terminal) read(prompt string) (string, error) {
 
 // Phone asks phone using terminal.
 func (t *Terminal) Phone(ctx context.Context) (string, error) {
-	return t.read("Phone:")
+	return t.read(t.printer.Sprintf(localization.PhoneDialogPrompt) + ":")
 }
 
 // Password asks password using terminal.
 func (t *Terminal) Password(ctx context.Context) (string, error) {
-	return t.read("Password:")
+	return t.read(t.printer.Sprintf(localization.PasswordDialogPrompt) + ":")
 }
 
 // Code asks code using terminal.
 func (t *Terminal) Code(ctx context.Context) (string, error) {
-	return t.read("Code:")
+	return t.read(t.printer.Sprintf(localization.CodeDialogPrompt) + ":")
 }
 
 // SignUp asks user info for sign up.
 func (t *Terminal) SignUp(ctx context.Context) (u telegram.UserInfo, err error) {
-	u.FirstName, err = t.read("First name:")
+	u.FirstName, err = t.read(t.printer.Sprintf(localization.FirstNameDialogPrompt) + ":")
 	if err != nil {
 		return u, xerrors.Errorf("read first name: %w", err)
 	}
 
-	u.LastName, err = t.read("Last name:")
+	u.LastName, err = t.read(t.printer.Sprintf(localization.SecondNameDialogPrompt) + ":")
 	if err != nil {
 		return u, xerrors.Errorf("read first name: %w", err)
 	}
@@ -76,12 +89,12 @@ func (t *Terminal) SignUp(ctx context.Context) (u telegram.UserInfo, err error) 
 // AcceptTermsOfService write terms of service received from Telegram and
 // asks to accept it.
 func (t *Terminal) AcceptTermsOfService(ctx context.Context, tos tg.HelpTermsOfService) error {
-	_, err := io.WriteString(t.Terminal, "Telegram requested sign up, user not found.\n\n"+tos.Text)
+	_, err := io.WriteString(t.Terminal, t.printer.Sprintf(localization.TOSDialogTitle)+"\n\n"+tos.Text)
 	if err != nil {
 		return xerrors.Errorf("write terms of service: %w", err)
 	}
 
-	t.Terminal.SetPrompt("Accept(Y/N):")
+	t.Terminal.SetPrompt(t.printer.Sprintf(localization.TOSDialogPrompt) + "(Y/N):")
 	defer t.Terminal.SetPrompt("")
 
 loop:
--- auth/terminal/code_test.go
+++ auth/terminal/code_test.go
@@ -7,6 +7,10 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
+
+	"github.com/gotd/contrib/auth/localization"
 )
 
 func TestTerminal(t *testing.T) {
@@ -14,7 +18,7 @@ func TestTerminal(t *testing.T) {
 	a := require.New(t)
 
 	var in, out bytes.Buffer
-	term := New(&in, &out)
+	term := New(&in, &out).WithPrinter(message.NewPrinter(language.English))
 	test := func(output, input string, call func(t *Terminal) (string, error)) {
 		in.WriteString(input + "\r")
 		phone, err := call(term)
@@ -24,13 +28,13 @@ func TestTerminal(t *testing.T) {
 		out.Reset()
 	}
 
-	test("Phone", "abc", func(t *Terminal) (string, error) {
+	test(localization.PhoneDialogPrompt, "abc", func(t *Terminal) (string, error) {
 		return t.Phone(ctx)
 	})
-	test("Password", "abc", func(t *Terminal) (string, error) {
+	test(localization.PasswordDialogPrompt, "abc", func(t *Terminal) (string, error) {
 		return t.Password(ctx)
 	})
-	test("Code", "abc", func(t *Terminal) (string, error) {
+	test(localization.CodeDialogPrompt, "abc", func(t *Terminal) (string, error) {
 		return t.Code(ctx)
 	})
 }
