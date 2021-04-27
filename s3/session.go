41e36d9765006e87e7bfd65783cd8f49c592f915
tdakkota <tanc13@yandex.ru>
2021-04-28 00:53:31 +0300

    feat(s3): add S3 support as session storage

---
+++ s3/session.go
@@ -0,0 +1,53 @@
+package s3
+
+import (
+	"bytes"
+	"context"
+	"io"
+
+	"github.com/minio/minio-go/v7"
+
+	"github.com/gotd/td/session"
+)
+
+var _ session.Storage = SessionStorage{}
+
+// SessionStorage is a MTProto session S3 storage.
+type SessionStorage struct {
+	client                 *minio.Client
+	bucketName, objectName string
+}
+
+// NewSessionStorage creates new SessionStorage.
+func NewSessionStorage(client *minio.Client, bucketName, objectName string) SessionStorage {
+	return SessionStorage{
+		client:     client,
+		bucketName: bucketName,
+		objectName: objectName,
+	}
+}
+
+// LoadSession implements session.Storage.
+func (s SessionStorage) LoadSession(ctx context.Context) ([]byte, error) {
+	obj, err := s.client.GetObject(ctx, s.bucketName, s.objectName, minio.GetObjectOptions{})
+	if err != nil {
+		return nil, err
+	}
+	return io.ReadAll(obj)
+}
+
+// StoreSession implements session.Storage.
+func (s SessionStorage) StoreSession(ctx context.Context, data []byte) error {
+	_, err := s.client.PutObject(ctx, s.bucketName, s.objectName,
+		bytes.NewReader(data), int64(len(data)),
+		minio.PutObjectOptions{
+			ContentType: "application/json",
+			NumThreads:  1,
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
