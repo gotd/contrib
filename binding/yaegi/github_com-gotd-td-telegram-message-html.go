e1a375fea3013cb8c80895819e09771db98e710c
Aleksandr Razumov <ernado@ya.ru>
2021-05-08 19:29:28 +0300

    chore: commit generated files

--- binding/yaegi/github_com-gotd-td-telegram-message-html.go
+++ binding/yaegi/github_com-gotd-td-telegram-message-html.go
@@ -3,17 +3,16 @@
 package yaegi
 
 import (
-	"reflect"
-
 	"github.com/gotd/td/telegram/message/html"
+	"reflect"
 )
 
 func init() {
 	Symbols["github.com/gotd/td/telegram/message/html"] = map[string]reflect.Value{
 		// function, constant and variable definitions
-		"Bytes":       reflect.ValueOf(html.Bytes),
-		"Format":      reflect.ValueOf(html.Format),
-		"ChunkSource": reflect.ValueOf(html.Reader),
-		"String":      reflect.ValueOf(html.String),
+		"Bytes":  reflect.ValueOf(html.Bytes),
+		"Format": reflect.ValueOf(html.Format),
+		"Reader": reflect.ValueOf(html.Reader),
+		"String": reflect.ValueOf(html.String),
 	}
 }
