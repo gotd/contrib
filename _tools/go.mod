2128c5f0997978a0afdfa499cfd6aa7927660186
rs <yuiop1955@mail.ru>
2021-05-10 20:47:51 +0500

    chore(_tools): _tools mod

---
+++ _tools/go.mod
@@ -0,0 +1,5 @@
+module github.com/gotd/contrib/_tools
+
+go 1.16
+
+require github.com/golang/mock v1.5.0
--- updates/internal/sequence/generate.go
+++ updates/internal/sequence/generate.go
@@ -1,3 +1,3 @@
 package sequence
 
-//go:generate mockgen -source=hooks.go -package=sequence -mock_names=hooks=MockedHooks -destination=hooks_mock_test.go
+//go:generate go run -modfile=../../../_tools/go.mod github.com/golang/mock/mockgen -source=hooks.go -package=sequence -mock_names=hooks=MockedHooks -destination=hooks_mock_test.go
