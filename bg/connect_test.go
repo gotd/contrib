5586273262d2c9d429e7f246f6c80575f0e0447c
tdakkota <tanc13@yandex.ru>
2021-07-16 15:44:58 +0300

    chore: make linter happy
    Use special type for WithValue key

--- bg/connect_test.go
+++ bg/connect_test.go
@@ -8,18 +8,20 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type testKey string
+
 type testClient struct {
 	tt *testing.T
 }
 
 func (t testClient) Run(ctx context.Context, f func(ctx context.Context) error) error {
-	assert.Equal(t.tt, "bar", ctx.Value("foo"))
+	assert.Equal(t.tt, "bar", ctx.Value(testKey("foo")))
 	return f(ctx)
 }
 
 func TestConnect(t *testing.T) {
 	ctx := context.Background()
-	ctx = context.WithValue(ctx, "foo", "bar")
+	ctx = context.WithValue(ctx, testKey("foo"), "bar")
 	stop, err := Connect(testClient{tt: t}, WithContext(ctx))
 	require.NoError(t, err)
 	require.NoError(t, stop())
